8da8b63fecd42ae7eb96e6fa275848d4eeeba07d
TheAdnan <adnankicin92@gmail.com>
2017-12-13 15:17:47 +0100

    get data from json

---
+++ extension/hadith/script.js
@@ -0,0 +1,29 @@
+$ = (queryString) => document.querySelector(queryString);
+
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(xhr.responseText);
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
+loadJSON('data/bukhari.json',
+         function(data) { 
+         	
+          },
+         function(xhr) { 
+         	return; 
+         }
+);
