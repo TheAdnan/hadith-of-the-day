fcc8b47cd720ac759f59bbed852664865e1d2d30
TheAdnan <adnankicin92@gmail.com>
2017-12-13 12:25:54 +0100

    numbering hadith

--- hadith-scraper.py
+++ hadith-scraper.py
@@ -17,6 +17,7 @@ r = requests.get(main_url)
 data = r.text
 supaPocetna = BeautifulSoup(data, "html.parser")
 book_number = 98
+counter = 0
 
 for url_range in range(1, book_number):
     urls = main_url + str(url_range) + ".html"
@@ -25,10 +26,12 @@ for url_range in range(1, book_number):
     data = r.text
     supa = BeautifulSoup(data, "html.parser")
     for hadith in supa.findAll("div", {"class": "english_hadith_full"}):
+        counter = counter + 1
         hit = hadith.text.encode('utf-8').decode('ascii', 'ignore')
         hit = str(hit)
-        hit.replace("<p>", "\n")
-        hit.replace("</p>", "\n")
+        hit.replace("<p>", " ")
+        hit.replace("</p>", " ")
+        result = {counter: hit}
         with open(hadithFolder + "/bukhari.json", 'a+') as outfile:
-            json.dump(hit, outfile)
+            json.dump(result, outfile)
 print("Finished scraping")
